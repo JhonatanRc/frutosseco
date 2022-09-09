4cdf47ada06d0a4846f68fd58823a265a82c89bc
Jhonatan Rivas <jjrc.12@outlook.com>
2022-09-08 19:46:45 -0500

    First commit

---
+++ Controllers/Contacto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demomvc.Controllers
+{
+    public class Contacto
+    {
+        public string Correo {get;set;}
+
+        public string Telefono {get;set;}
+
+        public string Nombre {get;set;}
+
+        public string Comentario {get;set;}
+    }
+}
