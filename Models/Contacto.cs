248549dae8160475685793edf749bd54a28cf034
Jhonatan Rivas <jjrc.12@outlook.com>
2022-09-09 17:22:07 -0500

    Agregando bootstrap

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -1,13 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace demomvc.Models
 {
     public class Contacto
     {
-        public string Correo { get; set; }
+        public string? Correo { get; set; }
 
-        public string Telefono {get;set;}
+        public string? Telefono { get; set; }
 
-        public string Nombre {get;set;}
+        public string? Nombre { get; set; }
 
-        public string Comentario {get;set;}
+        public string? Comentarios { get; set; }
     }
 }
